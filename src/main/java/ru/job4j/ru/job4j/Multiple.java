fe96b99e72fac5e5c19e2757632a40b2f668817c
Admin <alimdzhanova.arina@gmail.com>
2025-10-29 18:14:43 +0300

    Таблица умножения на 1/2

--- src/main/java/ru/job4j/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/ru/job4j/Multiple.java
@@ -3,8 +3,11 @@ package ru.job4j.ru.job4j;
 public class Multiple {
     public static void main (String[] args) {
         System.out.println("1 * 1 = 1");
+
         System.out.println("1 * 2 = 2");
+
         System.out.println("1 * 3 = 3");
+
         System.out.println("1 * 4 = 4");
         System.out.println("1 * 5 = 5");
         System.out.println("1 * 6 = 6");
