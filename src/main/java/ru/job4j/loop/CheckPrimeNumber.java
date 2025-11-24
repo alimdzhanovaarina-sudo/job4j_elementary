eb6087c532ac53681810b252d624af21aa893245
Admin <alimdzhanova.arina@gmail.com>
2025-11-24 16:41:48 +0300

    5.6. Простые числа [#505215]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -4,12 +4,10 @@ public class CheckPrimeNumber {
 
     public static boolean check(int number) {
         boolean prime = number > 1;
-        if (prime) {
-            for (int index = 2; index < number; index++) {
-                if (number % index == 0) {
-                    prime = false;
-                    break;
-                }
+        for (int index = 2; index < number; index++) {
+            if (number % index == 0) {
+                prime = false;
+                break;
             }
         }
         return prime;
@@ -19,6 +17,8 @@ public class CheckPrimeNumber {
         System.out.println("3 is prime: " + check(3));
         System.out.println("4 is prime: " + check(4));
         System.out.println("5 is prime: " + check(5));
+        System.out.println("1 is prime: " + check(1));
+        System.out.println("0 is prime: " + check(0));
     }
 }
 
---
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+
+    public static int calc(int finish) {
+        int count = 0;
+        for (int number = 2; number <= finish; number++) {
+            if (CheckPrimeNumber.check(number)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        System.out.println("Простых чисел до 5: " + calc(5));
+        System.out.println("Простых чисел до 10: " + calc(10));
+    }
+}
+
