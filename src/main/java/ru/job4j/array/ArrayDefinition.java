288c2b118cbce182bf0b780025c0d6f13aa53910
Admin <alimdzhanova.arina@gmail.com>
2025-11-25 18:16:06 +0300

    Заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,12 +1,19 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
-        System.out.println("Размер массива равен: " + ages.length);
+        System.out.println("Размер массива ages равен: " + ages.length);
         float[] prices = new float[40];
-        System.out.println("Размер массива равен: " + prices.length);
-        String[] surnames = new String[100500];
-        System.out.println("Размер массива равен: " + surnames.length);
+        System.out.println("Размер массива prices равен: " + prices.length);
+        String[] names = new String[5];
+        names[0] = "Katya";
+        names[1] = "Danya";
+        names[2] = "Rina";
+        names[3] = "Oleg";
+        names[4] = "Sergei";
+        System.out.println("Элементы массива names: " + Arrays.toString(names));
     }
 }
