3b8fa5dc14b2baa603c6774ea25534fd2e0f59a8
Admin <alimdzhanova.arina@gmail.com>
2025-12-02 13:23:51 +0300

    Заполнить массив степенями чисел [#505225 #555537]

--- src/main/java/ru/job4j/array/Square.java
+++ src/main/java/ru/job4j/array/Square.java
@@ -2,13 +2,6 @@ package ru.job4j.array;
 
 public class Square {
     public static int[] calculate(int bound) {
-        if (bound < 0) {
-            throw new IllegalArgumentException("Bound cannot be negative");
-        }
-        if (bound == 0) {
-            return new int[0];
-        }
-
         int[] result = new int[bound];
         for (int index = 0; index < bound; index++) {
             result[index] = index * index;
