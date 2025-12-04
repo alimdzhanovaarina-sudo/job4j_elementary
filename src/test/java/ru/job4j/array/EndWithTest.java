3bb24f7f2eea9b40565161bb56ab27dedecbb555
Admin <alimdzhanova.arina@gmail.com>
2025-12-04 16:18:17 +0300

    Слово заканчивается на ... [#505236]

--- src/test/java/ru/job4j/array/EndWithTest.java
+++ src/test/java/ru/job4j/array/EndWithTest.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class EndWithTest {
+public class EndsWithTest {
     @Test
     public void whenEndWithPrefixThenTrue() {
         char[] word = {'H', 'e', 'l', 'l', 'o'};
