464b665c34a3a2b4fa1b51a627bf32ca0464993a
Admin <alimdzhanova.arina@gmail.com>
2025-12-04 16:44:42 +0300

    Поиск индекса в диапазоне [#505237]

---
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+public class FindLoop {
+    public static int indexOf(int[] data, int element) {
+        int result = -1;
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == element) {
+                result = index;
+                break;
+            }
+        }
+        return result;
+    }
+    public static int indexInRange(int[] data, int element, int start, int finish) {
+        int result = -1;
+        for (int index = start; index <= finish; index++) {
+            if (data[index] == element) {
+                result = index;
+                break;
+            }
+        }
+        return result;
+    }
+}
