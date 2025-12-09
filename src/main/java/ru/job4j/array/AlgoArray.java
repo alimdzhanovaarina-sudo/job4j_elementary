474cacad2cea4ec0ce3330230d03ce21b324bda2
Admin <alimdzhanova.arina@gmail.com>
2025-12-09 19:15:57 +0300

    Упорядочить массив [#505228 #555568]

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -2,19 +2,10 @@ package ru.job4j.array;
 
 public class AlgoArray {
     public static void main(String[] args) {
-        // Наш изначальный список чисел:
         int[] array = new int[] {5, 3, 2, 1, 4};
-        for (int i = 0; i < array.length - 1; i++) {
-            for (int j = 0; j < array.length - 1 - i; j++) {
-                if (array[j] > array[j + 1]) {
-                    int temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                }
-            }
-        }
         for (int index = 0; index < array.length; index++) {
             System.out.println(array[index]);
         }
     }
 }
+
