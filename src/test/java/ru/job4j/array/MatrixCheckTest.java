31b3d0419405e026a3342c5f441e5ee333008e6c
Admin <alimdzhanova.arina@gmail.com>
2025-12-10 19:06:53 +0300

    6.7.1. Моно строка в матрице. [#505245 #555968]

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -5,89 +5,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class MatrixCheckTest {
     @Test
-    public void whenDiagonalFullX() {
+    public void whenHasMonoHorizontal() {
         char[][] input = {
-                {'X', ' ', ' '},
-                {' ', 'X', ' '},
-                {' ', ' ', 'X'},
+                {' ', ' ', ' '},
+                {'X', 'X', 'X'},
+                {' ', ' ', ' '},
         };
-        char[] result = MatrixCheck.extractDiagonal(input);
-        char[] expected = {'X', 'X', 'X'};
-        assertThat(result).containsExactly(expected);
-    }
-
-    @Test
-    public void whenDiagonalFullOne() {
-        char[][] input = {
-                {'1', ' ', ' '},
-                {' ', '1', ' '},
-                {' ', ' ', '1'},
-        };
-        char[] result = MatrixCheck.extractDiagonal(input);
-        char[] expected = {'1', '1', '1'};
-        assertThat(result).containsExactly(expected);
-    }
-
-    @Test
-    public void whenDiagonalMix() {
-        char[][] input = {
-                {'X', ' ', ' '},
-                {' ', 'Y', ' '},
-                {' ', ' ', 'Z'},
-        };
-        char[] result = MatrixCheck.extractDiagonal(input);
-        char[] expected = {'X', 'Y', 'Z'};
-        assertThat(result).containsExactly(expected);
-    }
-    @Test
-    public void whenDataMonoByTrueThenTrue() {
-        char[][] input = {
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '},
-        };
-        boolean result = MatrixCheck.isWin(input);
+        int row = 1;
+        boolean result = MatrixCheck.monoHorizontal(input, row);
         assertThat(result).isTrue();
     }
-
     @Test
-    public void whenDataNotMonoByTrueThenFalse() {
+    public void whenNoMono() {
         char[][] input = {
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', 'X', ' ', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '},
+                {'X', 'X', ' '}, // строка 0 содержит один пробел => должна вернуть false
+                {'X', 'X', 'X'},
+                {' ', ' ', 'X'}
         };
-        boolean result = MatrixCheck.isWin(input);
-        assertThat(result).isFalse();
-    }
-
-    @Test
-    public void whenDataHMonoByTrueThenTrue() {
-        char[][] input = {
-                {' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', ' ', ' '},
-                {'X', 'X', 'X', 'X', 'X'},
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '},
-        };
-        boolean result = MatrixCheck.isWin(input);
-        assertThat(result).isTrue();
-    }
-
-    @Test
-    public void whenDataDiagMonoIsTrueThenFalse() {
-        char[][] input = {
-                {'X', ' ', ' ', ' ', ' '},
-                {' ', 'X', ' ', ' ', ' '},
-                {' ', ' ', 'X', ' ', ' '},
-                {' ', ' ', ' ', 'X', ' '},
-                {' ', ' ', ' ', ' ', 'X'},
-        };
-        boolean result = MatrixCheck.isWin(input);
+        int row = 0;
+        boolean result = MatrixCheck.monoHorizontal(input, row);
         assertThat(result).isFalse();
     }
 }
