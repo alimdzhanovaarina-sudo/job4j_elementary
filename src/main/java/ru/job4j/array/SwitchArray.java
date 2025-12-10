8bdcbb762f6edc0a21b3b1d65b51808a1d2ed70b
Admin <alimdzhanova.arina@gmail.com>
2025-12-10 18:48:45 +0300

    Переставить элементы массива [#505230 #555572]

--- src/main/java/ru/job4j/array/SwitchArray.java
+++ src/main/java/ru/job4j/array/SwitchArray.java
@@ -17,6 +17,7 @@ public class SwitchArray {
         int[] numbers = new int[] {1, 2, 3, 4, 5, 6};
         printArray(numbers);
     }
+
     public static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
